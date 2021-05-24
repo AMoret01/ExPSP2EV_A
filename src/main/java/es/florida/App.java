dade4aa9beb856693003cf997afe1c7089c93ba6
almogo01 <amoretgomez@gmail.com>
2021-05-24 13:24:23 +0200

    Ejercicio 2 finalizado al 100%

--- src/main/java/es/florida/App.java
+++ src/main/java/es/florida/App.java
@@ -12,13 +12,8 @@ public class App {
     public static void main(String[] args) throws IOException {
 
         DronController controller = new DronController();
+        Socket dronSocket = controller.connect();
 
-        ServerSocket server = new ServerSocket(PORT);
-        Socket connection = server.accept();
-        InputStream input = connection.getInputStream();
-        OutputStream output = connection.getOutputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        PrintWriter printer = new PrintWriter(new OutputStreamWriter(output));
         controller.takeOff();
         esperar(1);
         controller.land();
@@ -27,12 +22,30 @@ public class App {
         esperar(1);
         controller.fireSecondaryWeapon();
         controller.shutDown();
-        String line;
 
-        while ((line = reader.readLine()) != null) {
-           System.out.println(line);
-        }
-        printer.println("Bienvenido...");
+        dronSocket.close();
+
+
+        //ServerSocket server = new ServerSocket(PORT);
+        //Socket connection = server.accept();
+        //InputStream input = connection.getInputStream();
+        //OutputStream output = connection.getOutputStream();
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        //PrintWriter printer = new PrintWriter(new OutputStreamWriter(output));
+        //controller.takeOff();
+        //esperar(1);
+        //controller.land();
+        //esperar(1);
+        //controller.firePrimaryCannon();
+        //esperar(1);
+        //controller.fireSecondaryWeapon();
+        //controller.shutDown();
+        //String line;
+//
+        //while ((line = reader.readLine()) != null) {
+        //   System.out.println(line);
+        //}
+        //printer.println("Bienvenido...");
     }
 
     public static void esperar(int segundos){
--- src/main/java/es/florida/DronController.java
+++ src/main/java/es/florida/DronController.java
@@ -1,33 +1,47 @@
 package es.florida;
 
 
+import javax.sound.sampled.Port;
+import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
-
+import java.net.Socket;
 public class DronController {
 
-    public Socket connect() {
-        // FIXME please implement solution or remove this method
-        return new Socket();
+    public static final int PORT = 9876;
+    private PrintWriter _printer = null;
+    public Socket connect() throws IOException {
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("localhost", PORT));
+
+        OutputStream output = socket.getOutputStream();
+        _printer = new PrintWriter(new OutputStreamWriter(output));
+        return socket;
     }
 
     public void takeOff() {
         System.out.println("Taking off...");
+        _printer.println("TAKE-OFF");
     }
 
     public void land() {
         System.out.println("Landing");
+        _printer.println("LAND");
     }
 
     public void firePrimaryCannon() {
         System.out.println("Ratatatatatatata!");
+        _printer.println("FIRE_P_W");
     }
 
     public void fireSecondaryWeapon() {
         System.out.println("Piñau! Piñau!");
+        _printer.println("FIRE_S_W");
     }
 
     public void shutDown() {
         System.out.println("Shutting down system...");
+        _printer.println("OFF");
     }
 
 }
